b0ace0bf0e72dfbcd6630a307be08f81b415195f
Delf Carl Boston <delf.boston@gmail.com>
2018-10-03 19:08:25 +0800

    Navigation Drawer (picker fragment,textfield fragment and toggle fragment)

---
+++ app/src/main/java/com/drawer/boston/drawerfragments/TextFieldDemo.java
@@ -0,0 +1,19 @@
+package com.drawer.boston.drawerfragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TextFieldDemo extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.textfield_fragment, container, false);
+
+        return layout;
+    }
+}
